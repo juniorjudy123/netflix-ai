df89ada640ea4438d69ab9a9ef674281ee3ab026
benoy_Judy <benoyjudy@gmail.com>
2024-05-21 12:45:50 +0530

    useNowPlayingMovies custom hook created inorder to clear up browseComponent

--- src/components/BrowseComponent.jsx
+++ src/components/BrowseComponent.jsx
@@ -1,25 +1,9 @@
-import React, { useEffect } from "react"
+import React from "react"
 import HeaderComponent from "./HeaderComponent"
-import { API_OPTIONS } from "../utils/constants"
-import { useDispatch } from "react-redux"
-import { addNowPlayingMovies } from "../utils/moviesSlice"
+import useNowPlayingMovies from "../customHooks/useNowPlayingMovies"
 
 const BrowseComponent = () => {
-	const dispatch = useDispatch()
-	const getNowPlayingMovies = async () => {
-		const data = await fetch(
-			"https://api.themoviedb.org/3/movie/now_playing?page=1",
-			API_OPTIONS
-		)
-		const json = await data.json()
-		console.log("movieList-", json.results)
-		dispatch(addNowPlayingMovies(json.results))
-	}
-
-	useEffect(() => {
-		getNowPlayingMovies()
-	}, [])
-
+	useNowPlayingMovies()
 	return (
 		<div>
 			<HeaderComponent />
